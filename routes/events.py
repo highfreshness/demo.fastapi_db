2eadfc31e13cbb973c4a3f9b3405a21be6aacf26
highfreshness <meteo0718@gmail.com>
2023-09-11 09:40:50 +0900

    SQL 서비스 데모

--- routes/events.py
+++ routes/events.py
@@ -1,5 +1,7 @@
-from fastapi import APIRouter, status, HTTPException, Body
-from models.events import Event
+from fastapi import APIRouter, status, HTTPException, Depends, Request
+from models.events import Event, EventUpdate
+from database.connection import get_session
+from sqlmodel import select
 from typing import List
 
 event_router = APIRouter(tags=["Events"])
@@ -8,15 +10,18 @@ events = []
 
 
 @event_router.get("/", response_model=List[Event])
-async def retrieve_all_events() -> List[Event]:
+async def retrieve_all_events(session=Depends(get_session)) -> List[Event]:
+    statement = select(Event)
+    events = session.exec(statement).all()
     return events
 
 
 @event_router.get("/{id}", response_model=Event)
-async def retrieve_event(id: int) -> Event:
-    for event in events:
-        if event.id == id:
-            return event
+async def retrieve_event(id: int, session=Depends(get_session)) -> Event:
+    event = session.get(Event, id)
+    if event:
+        return event
+
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
         detail="Event with supplied ID does not exist",
@@ -24,17 +29,20 @@ async def retrieve_event(id: int) -> Event:
 
 
 @event_router.post("/new")
-async def create_event(body: Event = Body(...)) -> dict:
-    events.append(body)
+async def create_event(new_event: Event, session=Depends(get_session)) -> dict:
+    session.add(new_event)
+    session.commit()
+    session.refresh(new_event)
     return {"message": "Event created sucessfully."}
 
 
-@event_router.delete("/{id}")
-async def delete_event(id: int) -> dict:
-    for event in events:
-        if event.id == id:
-            events.remove(event)
-            return {"message": "Event deleted successfully."}
+@event_router.delete("/delete/{id}")
+async def delete_event(id: int, session=Depends(get_session)) -> dict:
+    event = session.get(Event, id)
+    if event:
+        session.delete(event)
+        session.commit()
+        return {"message": "Event deleted successfully."}
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
         detail="Event with supplied ID does not exist",
@@ -45,3 +53,24 @@ async def delete_event(id: int) -> dict:
 async def delete_all_events() -> dict:
     events.clear()
     return {"message": "Events deleted successfully."}
+
+
+@event_router.put("/edit/{id}", response_model=Event)
+async def update_event(
+    id: int, new_data: EventUpdate, session=Depends(get_session)
+) -> Event:
+    event = session.get(Event, id)
+    if event:
+        event_data = new_data.dict(
+            exclude_unset=True
+        )  # Pydantic 모델에 없는 데이터 항목은 제외 후 dict생성
+        for key, value in event_data.items():
+            setattr(event, key, value)  # -> event[key]=value
+        session.add(event)
+        session.commit()
+        session.refresh(event)
+        return event
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Event with supplied ID does not exist.",
+    )
